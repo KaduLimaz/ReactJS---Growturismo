112a7f61d340e810522d38127bed379d82dc5691
Carlos Eduardo Sabino de Lima <121118484+KaduLimaz@users.noreply.github.com>
2024-04-02 16:09:53 -0300

    Quem Somos 100%
    Quem Somos 100%

--- growturismos/src/pages/QuemSomos.tsx
+++ growturismos/src/pages/QuemSomos.tsx
@@ -1,12 +1,27 @@
 import { BodyHomeContainer } from "../components/styled/BodyhomeContainer";
 import { DefaultLayount } from "../configs/layout/DefaultLayout";
+import QuemSomosImg from "../assets/aeroporto.svg";
 
 export function QuemSomos() {
 	return (
 		<>
 			<DefaultLayount>
 				<BodyHomeContainer>
-					<h1>Quem Somos</h1>
+					<div>
+						<img src={QuemSomosImg} alt="QuemSomosImg" />
+					</div>
+					<div>
+						<h1>| Quem Somos</h1>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa
+							totam neque consequuntur minima, quis, tenetur maxime!
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa
+							totam neque consequuntur minima, Et alias laboriosam sit sed
+							voluptatem!
+						</p>
+					</div>
 				</BodyHomeContainer>
 			</DefaultLayount>
 		</>
